77bf3c358f89e18e83a81f279a18c352bab17bee
Frederick Lawler <fred@fredlawl.com>
2024-03-02 11:58:23 -0600

    install boxes tmp;

--- playbooks/roles/install_kubernetes_boxes/scripts/add-box-if-missing.sh
+++ playbooks/roles/install_kubernetes_boxes/scripts/add-box-if-missing.sh
@@ -3,46 +3,48 @@
 # Vagrant lacks the ability to tell you if a box is present in one
 # single command line so we have to wrap this up for it.
 
-TMP_FILE=""
+echo "install box selected"
 
-box_search_finish()
-{
-	if [[ ! -z $TMP_FILE ]]; then
-		rm -f $TMP_FILE
-	fi
-}
+# TMP_FILE=""
 
-trap "box_search_finish" EXIT
+# box_search_finish()
+# {
+# 	if [[ ! -z $TMP_FILE ]]; then
+# 		rm -f $TMP_FILE
+# 	fi
+# }
 
-if [[ $# -ne 2 ]]; then
-	echo "Usage: $0 <box_name> <box_url>"
-	exit 1
-fi
+# trap "box_search_finish" EXIT
 
-BOX=$1
-BOX_URL=$2
+# if [[ $# -ne 2 ]]; then
+# 	echo "Usage: $0 <box_name> <box_url>"
+# 	exit 1
+# fi
 
-BOX_SEARCH="$1\s\+"
-TMP_FILE=$(mktemp)
+# BOX=$1
+# BOX_URL=$2
 
-vagrant box list > $TMP_FILE
-grep -q "$BOX_SEARCH" $TMP_FILE
-if [ $? -eq 0 ] ; then
-	exit 0
-else
-	NAME_ARGS=""
-	echo $BOX_URL | grep -q json$
-	# Sadly we cannot rename boxes in json format, so we must stick to
-	# what is given.
-	if [[ $? -ne 0 ]]; then
-		NAME_ARGS="--name $BOX"
-	fi
-	vagrant box add --provider=libvirt --insecure $NAME_ARGS $BOX_URL
-	RET=$?
-	# We use a special return value to indicate change to the
-	# ansible script, so that it can tell a change has occurred.
-	if [ $RET -eq 0 ]; then
-		exit 314
-	fi
-	exit $RET
-fi
+# BOX_SEARCH="$1\s\+"
+# TMP_FILE=$(mktemp)
+
+# vagrant box list > $TMP_FILE
+# grep -q "$BOX_SEARCH" $TMP_FILE
+# if [ $? -eq 0 ] ; then
+# 	exit 0
+# else
+# 	NAME_ARGS=""
+# 	echo $BOX_URL | grep -q json$
+# 	# Sadly we cannot rename boxes in json format, so we must stick to
+# 	# what is given.
+# 	if [[ $? -ne 0 ]]; then
+# 		NAME_ARGS="--name $BOX"
+# 	fi
+# 	vagrant box add --provider=libvirt --insecure $NAME_ARGS $BOX_URL
+# 	RET=$?
+# 	# We use a special return value to indicate change to the
+# 	# ansible script, so that it can tell a change has occurred.
+# 	if [ $RET -eq 0 ]; then
+# 		exit 314
+# 	fi
+# 	exit $RET
+# fi
